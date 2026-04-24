70a2424aab5084a26123af15b155ec955d22748c
Gargoil <emmanueledusei.k@gmail.com>
2026-04-24 11:02:42 +0000

    Fix: microservices can now communicate with RabbitMQ. GlobalExceptionHander has been imported properly

---
+++ shared/src/main/java/com/shared/definitions/SharedAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.shared.definitions;
+
+import com.shared.definitions.exception.GlobalExceptionHandler;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@AutoConfiguration
+@Import(GlobalExceptionHandler.class)
+public class SharedAutoConfiguration {}
